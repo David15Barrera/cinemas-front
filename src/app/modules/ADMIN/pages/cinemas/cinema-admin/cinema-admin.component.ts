477e39630b653c3138b30d81247c8c68e124fe22
David15Barrera <david1315barrera@gmail.com>
2025-10-28 01:36:24 -0600

    feat(globalCost): update global ad blocking cost

--- src/app/modules/ADMIN/pages/cinemas/cinema-admin/cinema-admin.component.ts
+++ src/app/modules/ADMIN/pages/cinemas/cinema-admin/cinema-admin.component.ts
@@ -12,77 +12,162 @@ import { finalize } from 'rxjs';
 })
 export class CinemaAdminComponent implements OnInit {
 
-  globalCost?: CostGlobalCinMed;
-  costInput: number = 0;
-  isSaving: boolean = false;
+  // Costo Diario Cine
+  globalCostCinema?: CostGlobalCinMed;
+  costInputCinema: number = 0;
+  isSavingCinema: boolean = false;
+  readonly COST_TYPE_CINEMA: CostGlobalCinMed['typeCost'] = 'COST_PER_DAY_CINEMA';
+  readonly COST_ID_CINEMA: string = 'cinema-cost-id';
 
-  readonly COST_TYPE: CostGlobalCinMed['typeCost'] = 'COST_PER_DAY_CINEMA';
-  readonly COST_ID: string = 'cinema-cost-id'; 
+  // Costo de Bloqueo de ADS
+  globalCostAds?: CostGlobalCinMed;
+  costInputAds: number = 0;
+  isSavingAds: boolean = false;
+  readonly COST_TYPE_ADS: CostGlobalCinMed['typeCost'] = 'AD_BLOCK';
+  readonly COST_ID_ADS: string = 'ads-block-cost-id';
+
 
   constructor(private costService: globalCostService) {}
 
   ngOnInit(): void {
-    this.loadGlobalCost();
+    this.loadGlobalCostCinema();
+    this.loadGlobalCostAds();
   }
 
-loadGlobalCost() {
-  this.costService.getGlobalCostCinema().subscribe({
-    next: (res) => {
-      this.globalCost = res;
-      this.costInput = res.cost;
-      console.log(' Costo cargado:', res);
-    },
-    error: (err) => console.error('Error cargando costo global', err)
-  });
-}
+  // --- Cine--
 
+  loadGlobalCostCinema() {
+    this.costService.getGlobalCostCinema().subscribe({
+      next: (res) => {
+        this.globalCostCinema = res;
+        this.costInputCinema = res.cost;
+        console.log(' Costo Cine cargado:', res);
+      },
+      error: (err) => console.error('Error cargando costo diario de cine', err)
+    });
+  }
 
-
-  saveCost() {
-    if (!this.costInput || this.costInput <= 0) {
-        alert('Por favor, ingrese un costo válido mayor a cero.');
+  saveCostCinema() {
+    if (!this.costInputCinema || this.costInputCinema <= 0) {
+        alert('Por favor, ingrese un costo válido mayor a cero para el Cine.');
         return;
     }
     
-    this.isSaving = true;
+    this.isSavingCinema = true;
+    const commonUpdate = (costToSave: CostGlobalCinMed) => {
+        return this.costService.updateGlobalCost(costToSave.id, costToSave).pipe(
+            finalize(() => this.isSavingCinema = false)
+        );
+    };
 
-    if (this.globalCost) {
+    const commonCreate = (costToSave: CostGlobalCinMed) => {
+        return this.costService.createGlobalCost(costToSave).pipe(
+            finalize(() => this.isSavingCinema = false)
+        );
+    };
+
+    if (this.globalCostCinema) {
       const updatedCost: CostGlobalCinMed = { 
-        ...this.globalCost, 
-        cost: this.costInput,
-        typeCost: this.COST_TYPE 
+        ...this.globalCostCinema, 
+        cost: this.costInputCinema,
+        typeCost: this.COST_TYPE_CINEMA 
       };
 
-      this.costService.updateGlobalCost(this.globalCost.id, updatedCost).pipe(
-        finalize(() => this.isSaving = false)
-      ).subscribe({
+      commonUpdate(updatedCost).subscribe({
         next: (res) => {
-          this.globalCost = res;
-          alert('Costo actualizado correctamente');
+          this.globalCostCinema = res;
+          alert('Costo Diario de Cine actualizado correctamente');
         },
-        error: (err) => console.error('Error actualizando costo', err)
+        error: (err) => console.error('Error actualizando costo diario de cine', err)
       });
       
     } else {
       const newCost: CostGlobalCinMed = { 
-        id: this.COST_ID,
-        cost: this.costInput,
-        typeCost: this.COST_TYPE
+        id: this.COST_ID_CINEMA,
+        cost: this.costInputCinema,
+        typeCost: this.COST_TYPE_CINEMA
       };
 
-      this.costService.createGlobalCost(newCost).pipe(
-        finalize(() => this.isSaving = false)
-      ).subscribe({
+      commonCreate(newCost).subscribe({
         next: (res) => {
-          this.globalCost = res;
-          alert('Costo creado correctamente');
+          this.globalCostCinema = res;
+          alert('Costo Diario de Cine creado correctamente');
         },
-        error: (err) => console.error('Error creando costo', err)
+        error: (err) => console.error('Error creando costo diario de cine', err)
       });
     }
   }
 
-  resetInput() {
-    this.costInput = this.globalCost?.cost || 0;
+  resetInputCinema() {
+    this.costInputCinema = this.globalCostCinema?.cost || 0;
+  }
+
+  // --- Bloqueo de ADS--
+
+  loadGlobalCostAds() {
+    this.costService.getGlobalCostAds().subscribe({
+      next: (res) => {
+        this.globalCostAds = res;
+        this.costInputAds = res.cost;
+        console.log(' Costo ADS cargado:', res);
+      },
+      error: (err) => console.error('Error cargando costo de bloqueo de ADS', err)
+    });
+  }
+
+  saveCostAds() {
+    if (!this.costInputAds || this.costInputAds <= 0) {
+        alert('Por favor, ingrese un costo válido mayor a cero para Bloqueo de ADS.');
+        return;
+    }
+    
+    this.isSavingAds = true;
+
+    const commonUpdate = (costToSave: CostGlobalCinMed) => {
+        return this.costService.updateGlobalCost(costToSave.id, costToSave).pipe(
+            finalize(() => this.isSavingAds = false)
+        );
+    };
+
+    const commonCreate = (costToSave: CostGlobalCinMed) => {
+        return this.costService.createGlobalCost(costToSave).pipe(
+            finalize(() => this.isSavingAds = false)
+        );
+    };
+
+    if (this.globalCostAds) {
+      const updatedCost: CostGlobalCinMed = { 
+        ...this.globalCostAds, 
+        cost: this.costInputAds,
+        typeCost: this.COST_TYPE_ADS 
+      };
+
+      commonUpdate(updatedCost).subscribe({
+        next: (res) => {
+          this.globalCostAds = res;
+          alert('Costo de Bloqueo de ADS actualizado correctamente');
+        },
+        error: (err) => console.error('Error actualizando costo de bloqueo de ADS', err)
+      });
+      
+    } else {
+      const newCost: CostGlobalCinMed = { 
+        id: this.COST_ID_ADS,
+        cost: this.costInputAds,
+        typeCost: this.COST_TYPE_ADS
+      };
+
+      commonCreate(newCost).subscribe({
+        next: (res) => {
+          this.globalCostAds = res;
+          alert('Costo de Bloqueo de ADS creado correctamente');
+        },
+        error: (err) => console.error('Error creando costo de bloqueo de ADS', err)
+      });
+    }
+  }
+
+  resetInputAds() {
+    this.costInputAds = this.globalCostAds?.cost || 0;
   }
 }
