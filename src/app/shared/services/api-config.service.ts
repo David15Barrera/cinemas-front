021630b1aea3d2291d0e47f70679fafe13ce253e
David15Barrera <david1315barrera@gmail.com>
2025-10-30 19:21:45 -0600

    enviroment

--- src/app/shared/services/api-config.service.ts
+++ src/app/shared/services/api-config.service.ts
@@ -6,10 +6,9 @@ import { environment } from '@environment/environment';
 })
 export class ApiConfigService {
   private readonly API_BASE = environment.API_ROOT;
-  private readonly API_IMAGEN = environment.API_URLUP;
   private readonly API_BASE_AUTH = `${this.API_BASE}/auth-identity/v1`;
   private readonly API_BASE_PROMOTION = `${this.API_BASE}/promotion/v1`;
-  public readonly API_IMAGES = `${this.API_IMAGEN}/images/v1`;
+  public readonly API_IMAGES = `${this.API_BASE}/images/v1`;
   public readonly API_BASE_AD = `${this.API_BASE}/ads/v1`;
   private readonly API_CINEMA_SERVICE = `${this.API_BASE}/cinema/v1`;
   private readonly API_MOVIE_SERVICE = `${this.API_BASE}/movie/v1`;
